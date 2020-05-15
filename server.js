c698f438191e1531c931193d88d543c65e12d623
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-15 22:18:34 +0700

    Completing backend

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/user"));
