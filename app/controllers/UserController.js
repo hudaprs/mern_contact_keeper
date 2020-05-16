d58f22cec6a5b714a23617f4f1a4ceffc37f6290
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-16 19:59:15 +0700

    register user with JWT token response

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,7 +1,9 @@
-const User = require("../models/User");
 const { success, error } = require("../helpers/responseApi");
 const { validationResult } = require("express-validator");
+const config = require("config");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 /**
  * @desc    Register user
@@ -35,7 +37,39 @@ exports.register = async (req, res) => {
 
     await user.save();
 
-    res.status(200).json(success("User registered", user, res.statusCode));
+    const payload = {
+      user: {
+        id: user.id,
+        name: user.name,
+      },
+    };
+
+    // Assign token from JWT
+    jwt.sign(
+      payload,
+      config.get("jwtSecret"),
+      {
+        expiresIn: 36000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(201).json(
+          success(
+            "User registered",
+            {
+              user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                date: user.date,
+              },
+              token,
+            },
+            res.statusCode
+          )
+        );
+      }
+    );
   } catch (err) {
     console.error(err.message);
     res.status(500).json(error("Server error"));
