e771b864456c117194a679c39120bf20e1dd00f6
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-18 19:12:46 +0700

    cleaning up, add navbar and routing

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p className="my-1">This app for keeping each other contact</p>
+      <p className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
