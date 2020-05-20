93a25da96d04a4ca59659fa75d2ee04780277755
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-21 02:06:49 +0700

    final fix

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const 
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
