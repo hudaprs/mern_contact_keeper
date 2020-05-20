ab9caa7a54d399b8d527a228375111eac5d0cb7d
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-21 02:04:54 +0700

    fix path function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const 
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
@@ -22,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
-    res.sendFile(path(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
