c80101f19cb202a29876c36cd76708455b6213fc
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-21 01:50:31 +0700

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
@@ -14,5 +15,15 @@ connectDB();
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/user"));
 app.use("/api/contacts", require("./routes/contact"));
+
+// Deploy
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
